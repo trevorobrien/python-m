c7cc1bc1c11d380a6437c69a726f0aa5b4a0cbb6
Trevor O'Brien <obrientrevor@yahoo.co.uk>
2020-04-20 14:52:45 +0100

    Upate text

--- hello.py
+++ hello.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 port = int(os.environ.get("PORT", 5000))
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized and deployed to Heroku'
+    return 'Flask Dockerized and deployed to Heroku A'
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=port)
